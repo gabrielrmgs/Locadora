2439bb87b9156feafabc72faa2bc3b0b8b692d14
gabrielrmgs <gabrielrdes@outlook.com>
2024-04-23 17:09:17 -0300

    Controller Filme

--- src/main/java/com/gabriel/locadora/controllers/FilmeController.java
+++ src/main/java/com/gabriel/locadora/controllers/FilmeController.java
@@ -4,7 +4,6 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,52 +14,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.gabriel.locadora.models.Cliente;
-import com.gabriel.locadora.models.Cliente.CreateCliente;
-import com.gabriel.locadora.models.Cliente.UpdateCliente;
-import com.gabriel.locadora.services.ClienteService;
-
-import jakarta.validation.Valid;
+import com.gabriel.locadora.models.Filme;
+import com.gabriel.locadora.services.FilmeService;
 
 @RestController
-@RequestMapping("/cliente")
-@Validated
-public class ClienteController {
-
+@RequestMapping("/filme")
+public class FilmeController {
+    
     @Autowired
-    private ClienteService clienteService;
+    private FilmeService filmeService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<Cliente> findById(@PathVariable Long id) {
-        Cliente obj = this.clienteService.findById(id);
+    public ResponseEntity<Filme> findById(@PathVariable long id) {
+        Filme obj = this.filmeService.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 
     @PostMapping
-    @Validated(CreateCliente.class)
-    public ResponseEntity<Void> create(@Valid @RequestBody Cliente obj) {
-
-        this.clienteService.criarCliente(obj);
+    public ResponseEntity<Void> create(@RequestBody Filme obj) {
+        this.filmeService.criarFilme(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
-
     }
 
     @PutMapping("/{id}")
-    @Validated(UpdateCliente.class)
-    public ResponseEntity<Void> update(@Valid @RequestBody Cliente obj, @PathVariable Long id){
-        
+    public ResponseEntity<Void> update(@RequestBody Filme obj, @PathVariable Long id) {
         obj.setId(id);
-        this.clienteService.atualizarCliente(obj);
+        this.filmeService.atualizarFilme(obj);
         return ResponseEntity.noContent().build();
-
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
-
-        this.clienteService.excluirCliente(id);
+        this.filmeService.deletarFilme(id);
         return ResponseEntity.noContent().build();
     }
-
 }
